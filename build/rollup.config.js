957d45a3122a028ff84a2189e1af82d03e5dfb1d
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-09-04 09:42:47 +0200

    enable development mode commands

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -69,6 +69,9 @@ export default inputs.map(input => {
             format: 'amd',
             name
         },
+        watch: {
+            clearScreen : false
+        },
         external: [
             ...localExternals,
             'handlebars',

2b3019d6b5b0fe4d6d874a2774075fdfb04d2760
lecosson <lecosson@gmail.com>
2020-04-23 19:02:58 +0300

    refactoring: dev.year, css placement

--- src/plugins/content/accessibility/jumplinks/jumplinks.js
+++ src/plugins/content/accessibility/jumplinks/jumplinks.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
  */
 
 /**
@@ -25,7 +25,6 @@ import $ from 'jquery';
 import _ from 'lodash';
 import component from 'ui/component';
 import jumplinksTpl from 'taoQtiTest/runner/plugins/content/accessibility/jumplinks/jumplinks.tpl';
-import 'taoQtiTest/runner/plugins/content/accessibility/jumplinks/jumplinks.css';
 
 /**
  * Default config values, see below.

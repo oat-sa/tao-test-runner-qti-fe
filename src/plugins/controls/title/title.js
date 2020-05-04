adcd63d79ee5f4c309c8d9d11b5928eced981d90
Anton Tsymuk <atsymuk@gmail.com>
2020-05-04 18:09:21 +0300

    update title plugin to include invisible timers

--- src/plugins/controls/title/title.js
+++ src/plugins/controls/title/title.js
@@ -22,55 +22,183 @@
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
 import $ from 'jquery';
+import __ from 'i18n';
+import _ from 'lodash';
 import pluginFactory from 'taoTests/runner/plugin';
 import titleTpl from 'taoQtiTest/runner/plugins/controls/title/title.tpl';
 import mapHelper from 'taoQtiTest/runner/helpers/map';
+import moment from 'moment';
+import statsHelper from 'taoQtiTest/runner/helpers/stats';
+
+const precision = 1000;
 
 export default pluginFactory({
     name: 'title',
     init: function init() {
-        var self = this;
-        var testRunner = this.getTestRunner();
-        var testMap = testRunner.getTestMap();
+        const testRunner = this.getTestRunner();
+        const testMap = testRunner.getTestMap();
 
-        var createElement = function() {
+        const updateTitles = () => {
             const testContext = testRunner.getTestContext();
+            const currentPart = mapHelper.getItemPart(
+                testMap,
+                testContext.itemPosition
+            );
+            const currentItem = mapHelper.getItem(
+                testMap,
+                testContext.itemIdentifier
+            );
 
-            const titles = [
-                {
-                    control: 'qti-test-title',
-                    text: testMap.title || ''
-                }
-            ];
+            // update test title
+            this.titles.test.$title.text(testMap.title).show();
 
+            // update part title
+            if (currentPart) {
+                this.titles.testPart.$title
+                    .text(` - ${currentPart.label}`)
+                    .show();
+            }
+
+            // update section title
             //@deprecated the following block seems to
             //be very specific and need to be reworked
             if (testContext.isDeepestSectionVisible) {
                 const section = mapHelper.getItemSection(
-                    testRunner.getTestMap(),
+                    testMap,
                     testContext.itemPosition
                 );
-                titles.push({
-                    //WTF !? isn't it the section title... ?
-                    control: 'qti-test-position',
-                    //testContext.sectionTitle is kept only for backward compat
-                    text: section.title || testContext.sectionTitle
-                });
+
+                //testContext.sectionTitle is kept only for backward compat
+                this.titles.section.$title
+                    .text(` - ${section.label || testContext.sectionTitle}`)
+                    .show();
             }
 
-            return $(titleTpl({ titles: titles }));
+            // update item title
+            this.titles.item.$title
+                .text(` - ${currentItem.label}`)
+                .show();
         };
 
-        this.$element = createElement();
+        testRunner
+            .after('renderitem', () => {
+                updateTitles();
 
-        testRunner.after('renderitem', function() {
-            var $element = createElement();
-            self.$element.replaceWith($element);
-            self.$element = $element;
-        });
+                _.forOwn(this.titles, (options, scope) => {
+                    if (scope === 'item') {
+                        return;
+                    }
+
+                    this.titles[scope].stats = statsHelper.getInstantStats(scope, testRunner);
+                });
+            })
+            .on('timertick', (remainingTime, scope) => {
+                const title = this.titles[scope];
+
+                if (!title) {
+                    return;
+                }
+
+                const {
+                    $timer,
+                    hours: currentHours,
+                    minutes: currentMinutes,
+                    stats,
+                    unansweredQuestions: currentUnansweredQuestions,
+                } = this.titles[scope];
+                const time = moment.duration(remainingTime / precision, 'seconds');
+                const hours = time.get('hours');
+                const minutes = time.get('minutes');
+                let timerMessage = '';
+
+                // check if notification should be updated
+                if (
+                    currentHours !== hours
+                    || currentMinutes !== minutes
+                    || (stats && ((stats.questions - stats.answered) !== currentUnansweredQuestions))
+                ) {
+                    const unansweredQuestions = stats && (stats.questions - stats.answered);
+
+                    this.titles[scope].hours = hours;
+                    this.titles[scope].minutes = minutes;
+                    this.titles[scope].unansweredQuestions = stats && (stats.questions - stats.answered);
+
+                    if (unansweredQuestions) {
+                        if (hours > 0) {
+                            timerMessage = __(
+                                '%sh %sm to answer remaining %s questions.',
+                                hours,
+                                minutes,
+                                unansweredQuestions
+                            );
+                        } else if (minutes > 0) {
+                            timerMessage = __(
+                                '%sm to answer remaining %s questions.',
+                                minutes,
+                                unansweredQuestions
+                            );
+                        } else {
+                            timerMessage = __(
+                                'Less than one minute to answer remaining %s questions.',
+                                unansweredQuestions
+                            );
+                        }
+                    } else {
+                        if (hours > 0) {
+                            timerMessage = __(
+                                '%sh %sm to answer the current question.',
+                                hours,
+                                minutes
+                            );
+                        } else if (minutes > 0) {
+                            timerMessage = __(
+                                '%sm to answer the current question.',
+                                minutes
+                            );
+                        } else {
+                            timerMessage = __(
+                                'Less than one minute to answer the current question.'
+                            );
+                        }
+                    }
+
+                    $timer.text(timerMessage);
+                }
+            })
+            .on('unloaditem', () => {
+                $('qti-controls', this.$element).hide();
+            });
     },
     render: function render() {
-        var $container = this.getAreaBroker().getControlArea();
+        const $container = this.getAreaBroker().getControlArea();
+        this.titles = {
+            test: {
+                attribute: 'qti-test-title',
+                className: '',
+            },
+            testPart: {
+                attribute: 'qti-test-part-title',
+                className: 'visible-hidden',
+            },
+            section: {
+                attribute: 'qti-test-position',
+                className: '',
+            },
+            item: {
+                attribute: 'qti-test-item-title',
+                className: 'visible-hidden',
+            },
+        };
+        this.$element = $(titleTpl({ titles: _.values(this.titles) }));
+
+        // hide titles by default
+        $('qti-controls', this.$element).hide();
+
         $container.append(this.$element);
-    }
+
+        _.forOwn(this.titles, ({ attribute }, scope) => {
+            this.titles[scope].$title = $container.find(`[data-control="${attribute}"]`);
+            this.titles[scope].$timer = $container.find(`[data-control="${attribute}-timer"]`);
+        });
+    },
 });

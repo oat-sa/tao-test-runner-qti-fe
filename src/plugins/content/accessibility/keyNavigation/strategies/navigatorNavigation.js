e2dea954592eb541a78d6bf8974e03bd3f1ebf4d
Arthur Paveliev <arthur.paveliev@taotesting.com>
2020-05-18 14:06:02 +0300

    Fixed review panel navigation strategy

--- src/plugins/content/accessibility/keyNavigation/strategies/navigatorNavigation.js
+++ src/plugins/content/accessibility/keyNavigation/strategies/navigatorNavigation.js
@@ -73,25 +73,8 @@ export default {
                 });
                 setupClickableNavigator(testStatusNavigation);
 
-                if (config.keyNextContent) {
-                    testStatusNavigation.on(config.keyNextContent, elem => {
-                        if (allowedToNavigateFrom(elem) && filtersNavigator) {
-                            filtersNavigator.focus();
-                        }
-                    });
-                }
-                if (config.keyPrevContent) {
-                    testStatusNavigation.on(config.keyPrevContent, elem => {
-                        if (allowedToNavigateFrom(elem) && itemsNavigator) {
-                            _.defer(() => {
-                                itemsNavigator.last();
-                            });
-                        }
-                    });
-                }
-
-                this.keyNavigators.push(testStatusNavigation);
-                this.managedNavigators.push(testStatusNavigation);
+                this.keyNavigators.push(testStatusNavigation)
+                // this.managedNavigators.push(testStatusNavigation);
             }
 
             $filters = $navigator.find('.qti-navigator-filters .qti-navigator-filter');
@@ -142,19 +125,15 @@ export default {
                 }
                 if (config.keyPrevContent) {
                     filtersNavigator.on(config.keyPrevContent, elem => {
-                        if (allowedToNavigateFrom(elem) && testStatusNavigation) {
-                            testStatusNavigation.focus();
-                        } else if (allowedToNavigateFrom(elem) && itemsNavigator) {
+                        if (allowedToNavigateFrom(elem) && itemsNavigator) {
                             _.defer(() => {
                                 itemsNavigator.last();
-                            })
+                            });
                         }
                     });
                 }
 
-                if (!testStatusNavigation || !config.keyNextTab || !config.keyPrevTab) {
-                    this.keyNavigators.push(filtersNavigator);
-                }
+                this.keyNavigators.push(filtersNavigator);
                 this.managedNavigators.push(filtersNavigator);
             }
 
@@ -201,13 +180,7 @@ export default {
                 setupClickableNavigator(itemsNavigator);
 
                 if (config.keepState) {
-                    itemsNavigator.on('upperbound', () => {
-                        if (testStatusNavigation) {
-                            testStatusNavigation.focus();
-                        }
-                    });
-
-                    itemsNavigator.on('lowerbound', () => {
+                    itemsNavigator.on('lowerbound upperbound', () => {
                         if (filtersNavigator) {
                             filtersNavigator.focus();
                         }
@@ -216,9 +189,7 @@ export default {
 
                 if (config.keyNextTab && config.keyPrevTab) {
                     itemsNavigator.on(config.keyNextTab, function (elem) {
-                        if (allowedToNavigateFrom(elem) && testStatusNavigation) {
-                            testStatusNavigation.focus();
-                        } else if (allowedToNavigateFrom(elem) && filtersNavigator) {
+                        if (allowedToNavigateFrom(elem) && filtersNavigator) {
                             filtersNavigator.focus().next();
                         }
                     });

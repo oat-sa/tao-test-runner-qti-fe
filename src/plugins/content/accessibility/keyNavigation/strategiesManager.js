28f8892fc2dffd27709c9c2995e72a21304a9df4
jsconan <jsconan@gmail.com>
2020-05-27 17:38:51 +0200

    feat: add option to apply WCAG behavior on radio buttons and similar elements (off by default)

--- src/plugins/content/accessibility/keyNavigation/strategiesManager.js
+++ src/plugins/content/accessibility/keyNavigation/strategiesManager.js
@@ -34,6 +34,7 @@ import * as strategies from 'taoQtiTest/runner/plugins/content/accessibility/key
  * Defines the config structure for the navigation strategies
  * @typedef {Object} keyNavigationStrategyConfig
  * @property {Boolean} autoFocus - auto select the main action when available in a group
+ * @property {Boolean} wcagBehavior - apply WCAG recommended behavior for radio buttons and similar elements
  * @property {Boolean} keepState - for strategies able to keep the state, allow to keep the position of the focused
  * element when moving away from the group and restore it when the group retrieves the focus
  * @property {Boolean} propagateTab - propagate the Tab key to the upper level

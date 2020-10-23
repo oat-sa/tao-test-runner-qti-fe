44fb1cc374cd60d95aea8f551f3ef0ffe67246d1
Oleg.Belanovich <oleg.belanovich@1pt.com>
2020-10-23 10:55:16 +0300

    Fix import

--- src/plugins/content/accessibility/keyNavigation/strategies/textblockNavigation.js
+++ src/plugins/content/accessibility/keyNavigation/strategies/textblockNavigation.js
@@ -1,110 +1,113 @@
-define(['jquery', 'i18n', 'ui/keyNavigation/navigator', 'ui/keyNavigation/navigableDomElement', 'taoQtiTest/runner/plugins/content/accessibility/keyNavigation/helpers'], function ($, __, keyNavigator, navigableDomElement, helpers) { 'use strict';
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 Open Assessment Technologies SA ;
+ */
 
-    $ = $ && Object.prototype.hasOwnProperty.call($, 'default') ? $['default'] : $;
-    __ = __ && Object.prototype.hasOwnProperty.call(__, 'default') ? __['default'] : __;
-    keyNavigator = keyNavigator && Object.prototype.hasOwnProperty.call(keyNavigator, 'default') ? keyNavigator['default'] : keyNavigator;
-    navigableDomElement = navigableDomElement && Object.prototype.hasOwnProperty.call(navigableDomElement, 'default') ? navigableDomElement['default'] : navigableDomElement;
+import $ from 'jquery';
+import __ from 'i18n';
+import keyNavigator from 'ui/keyNavigation/navigator';
+import navigableDomElement from 'ui/keyNavigation/navigableDomElement';
+import {
+  setupItemsNavigator
+} from 'taoQtiTest/runner/plugins/content/accessibility/keyNavigation/helpers';
 
-    /**
-     * This program is free software; you can redistribute it and/or
-     * modify it under the terms of the GNU General Public License
-     * as published by the Free Software Foundation; under version 2
-     * of the License (non-upgradable).
-     *
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-     *
-     * You should have received a copy of the GNU General Public License
-     * along with this program; if not, write to the Free Software
-     * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-     *
-     * Copyright (c) 2020 Open Assessment Technologies SA ;
-     */
-    /**
-    * The identifier the keyNavigator group
-    * @type {String}
-    */
+/**
+ * The identifier the keyNavigator group
+ * @type {String}
+ */
+const groupId = 'textblock-element-navigation-group';
 
-    var groupId = 'textblock-element-navigation-group';
-    /**
-     * Key navigator strategy applying on textblock items with scrollbar.
-     * Navigable item content are body elements with the special class "textBlock-wrap".
-     * @type {Object} keyNavigationStrategy
-     */
+/**
+ * Key navigator strategy applying on textblock items with scrollbar.
+ * Navigable item content are body elements with the special class "textBlock-wrap".
+ * @type {Object} keyNavigationStrategy
+ */
+export default {
+  name: 'textblock',
 
-    var textblockNavigation = {
-      name: 'textblock',
+  /**
+   * Builds the item navigation strategy.
+   *
+   * @returns {keyNavigationStrategy}
+   */
+  init() {
+    const config = this.getConfig();
+    const $content = this.getTestRunner().getAreaBroker().getContentArea();
 
-      /**
-       * Builds the item navigation strategy.
-       *
-       * @returns {keyNavigationStrategy}
-       */
-      init: function init() {
-        var _this = this;
+    this.keyNavigators = [];
 
-        var config = this.getConfig();
-        var $content = this.getTestRunner().getAreaBroker().getContentArea();
-        this.keyNavigators = []; // decorate isEnabled navigableDomElement method to check for dom node height
+    // decorate isEnabled navigableDomElement method to check for dom node height
+    const isEnabledDecorator = element => {
+      const originalIsEnabled = element.isEnabled;
 
-        var isEnabledDecorator = function isEnabledDecorator(element) {
-          var originalIsEnabled = element.isEnabled;
+      element.isEnabled = function isEnabled() {
+        if (originalIsEnabled.call(this)) {
+          const node = this.getElement().get(0);
+          var isScrolling = element.attr('data-scrolling');
 
-          element.isEnabled = function isEnabled() {
-            if (originalIsEnabled.call(this)) {
-              var node = this.getElement().get(0);
-              var isScrolling = element.attr('data-scrolling');
+          return isScrolling && (node.scrollHeight > node.clientHeight);
+        }
 
-              return isScrolling && node.scrollHeight > node.clientHeight;
-            }
+        return false;
+      };
 
-            return false;
-          };
+      return element;
+    };
 
-          return element;
-        };
+    $content
+        .find('.textBlock-wrap')
+        .addClass('key-navigation-scrollable')
+        .each((i, el) => {
+          const $element = $(el);
+          const elements = navigableDomElement.createFromDoms($element)
+              .map(isEnabledDecorator);
 
-        $content.find('.textBlock-wrap').addClass('key-navigation-scrollable').each(function (i, el) {
-          var $element = $(el);
-          var elements = navigableDomElement.createFromDoms($element).map(isEnabledDecorator); // assign aria attributes
-
+          // assign aria attributes
           $element.attr('aria-label', __('TextBlock'));
-          var navigator = keyNavigator({
-            id: "".concat(groupId, "-").concat(i),
-            elements: elements,
+
+          const navigator = keyNavigator({
+            id: `${groupId}-${i}`,
+            elements,
             group: $element,
             propagateTab: false
           });
-          helpers.setupItemsNavigator(navigator, config);
 
-          _this.keyNavigators.push(navigator);
+          setupItemsNavigator(navigator, config);
+          this.keyNavigators.push(navigator);
         });
-        return this;
-      },
 
-      /**
-       * Gets the list of applied navigators
-       * @returns {keyNavigator[]}
-       */
-      getNavigators: function getNavigators() {
-        return this.keyNavigators;
-      },
+    return this;
+  },
 
-      /**
-       * Tears down the keyNavigator strategy
-       * @returns {keyNavigationStrategy}
-       */
-      destroy: function destroy() {
-        this.keyNavigators.forEach(function (navigator) {
-          return navigator.destroy();
-        });
-        this.keyNavigators = [];
-        return this;
-      }
-    };
+  /**
+   * Gets the list of applied navigators
+   * @returns {keyNavigator[]}
+   */
+  getNavigators() {
+    return this.keyNavigators;
+  },
 
-    return textblockNavigation;
+  /**
+   * Tears down the keyNavigator strategy
+   * @returns {keyNavigationStrategy}
+   */
+  destroy() {
+    this.keyNavigators.forEach(navigator => navigator.destroy());
+    this.keyNavigators = [];
 
-});
+    return this;
+  }
+};

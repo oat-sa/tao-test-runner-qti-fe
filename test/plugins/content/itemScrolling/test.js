7c099b1b0458df4082ec86c1eba2f0620f86df18
Oleg.Belanovich <oleg.belanovich@1pt.com>
2020-11-05 14:41:33 +0300

    Work around test

--- test/plugins/content/itemScrolling/test.js
+++ test/plugins/content/itemScrolling/test.js
@@ -33,10 +33,95 @@ define([
 ], function ($, runnerFactory, providerMock, qtiItemRunner, itemData, QtiLoader, QtiRenderer, pluginFactory) {
     'use strict';
 
-    let pluginApi;
-    let runner;
-    const containerId = 'item-container';
-    const providerName = 'mock';
+    itemData = {
+        "identifier": "i5fa1562041342745bf1f971f9ec37c",
+        "serial": "item_5fa2a1c8767ad740071033",
+        "qtiClass": "assessmentItem",
+        "attributes": {
+            "identifier": "i5fa1562041342745bf1f971f9ec37c",
+            "title": "Item 4",
+            "label": "Item 4",
+            "xml:lang": "en-US",
+            "adaptive": false,
+            "timeDependent": false,
+            "toolName": "TAO",
+            "toolVersion": "3.4.0-sprint139",
+            "class": ""
+        },
+        "body": {
+            "serial": "container_containeritembody_5fa2a1c876775453516340",
+            "body": "\n    <div class=\"grid-row\">\n      <div class=\"col-12\">\n        <div class=\"text-block-wrap inner\" data-scrolling=\"true\" data-scrolling-height=\"50\">\n          <p>Lorem ipsum dolor sit amet, consectetur adipisicing ...<\/p>\n          <p>\u00a0<\/p>\n          <p>\u00a0<\/p>\n          <div class=\"text-block-wrap outer\" data-scrolling=\"true\" data-scrolling-height=\"50\">\n            {{xinclude_5fa2a1c87b4e9722396069}}\n          <\/div>\n        <\/div>\n      <\/div>\n    <\/div>\n  ",
+            "elements": {
+                "xinclude_5fa2a1c87b4e9722396069": {
+                    "serial": "xinclude_5fa2a1c87b4e9722396069",
+                    "qtiClass": "include",
+                    "attributes": {
+                        "href": "taomedia:\/\/mediamanager\/https_2_tao-inst_0_docker_0_localhost_1_ontologies_1_tao_0_rdf_3_i5f96c7ef552268fc0562774b9a42b6"
+                    },
+                    "body": {
+                        "serial": "container_containerstatic_5fa2a1c87bd41621570395",
+                        "body": "\n    \n        <div class=\"grid-row\">\n    <div class=\"col-12\">test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long\n<div>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n\n<p>test sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then longtest sstring long very long longer then long<\/p>\n<\/div><\/div>\n<\/div>\n    \n",
+                        "elements": {},
+                        "debug": {
+                            "relatedItem": "item_5fa2a1c8767ad740071033"
+                        }
+                    },
+                    "debug": {
+                        "relatedItem": "item_5fa2a1c8767ad740071033"
+                    }
+                }
+            },
+            "debug": {
+                "relatedItem": "item_5fa2a1c8767ad740071033"
+            }
+        },
+        "debug": {
+            "relatedItem": "item_5fa2a1c8767ad740071033"
+        },
+        "namespaces": {
+            "": "http:\/\/www.imsglobal.org\/xsd\/imsqti_v2p2",
+            "m": "http:\/\/www.w3.org\/1998\/Math\/MathML",
+            "xi": "http:\/\/www.w3.org\/2001\/XInclude",
+            "xsi": "http:\/\/www.w3.org\/2001\/XMLSchema-instance"
+        },
+        "schemaLocations": {
+            "http:\/\/www.imsglobal.org\/xsd\/imsqti_v2p2": "http:\/\/www.imsglobal.org\/xsd\/qti\/qtiv2p2\/imsqti_v2p2.xsd"
+        },
+        "stylesheets": {
+            "stylesheet_5fa2a1c879ae5712640514": {
+                "serial": "stylesheet_5fa2a1c879ae5712640514",
+                "qtiClass": "stylesheet",
+                "attributes": {
+                    "href": "style\/custom\/tao-user-styles.css",
+                    "type": "text\/css",
+                    "media": "all",
+                    "title": ""
+                },
+                "debug": {
+                    "relatedItem": "item_5fa2a1c8767ad740071033"
+                }
+            }
+        },
+        "outcomes": {},
+        "responses": {},
+        "feedbacks": {},
+        "responseProcessing": {
+            "serial": "response_templatesdriven_5fa2a1c87e381270995749",
+            "qtiClass": "responseProcessing",
+            "attributes": {},
+            "debug": {
+                "relatedItem": "item_5fa2a1c8767ad740071033"
+            },
+            "processingType": "templateDriven",
+            "responseRules": []
+        },
+        "apipAccessibility": ""
+    };
+
+    var pluginApi;
+    var runner;
+    var containerId = 'item-container';
+    var providerName = 'mock';
     let testRunner;
 
     runnerFactory.registerProvider(providerName, providerMock());
@@ -71,8 +156,8 @@ define([
     ];
 
     QUnit.cases.init(pluginApi).test('plugin API ', function (data, assert) {
-        runner = runnerFactory(providerName);
-        const plugin = pluginFactory(runner);
+        var runner = runnerFactory(providerName);
+        var plugin = pluginFactory(runner);
         assert.equal(
             typeof plugin[data.name],
             'function',
@@ -89,7 +174,7 @@ define([
     });
 
     QUnit.test('itemScrolling data loading', function (assert) {
-        const ready = assert.async();
+        var ready = assert.async();
         assert.expect(2);
 
         runner = qtiItemRunner('qti', itemData)
@@ -111,7 +196,7 @@ define([
     });
 
     QUnit.test('Item rendering', function (assert) {
-        const ready = assert.async();
+        var ready = assert.async();
         assert.expect(3);
 
         const container = document.getElementById(containerId);
@@ -141,17 +226,17 @@ define([
     let item;
 
     QUnit.test('itemScrolling rendering', function (assert) {
-        const ready = assert.async();
-        let renderer;
+        var ready = assert.async();
+        var renderer;
 
         assert.ok(typeof 'string' === 'string', 'The renderer creates a string');
 
         renderer = new QtiRenderer({baseUrl: './'});
         new QtiLoader().loadItemData(itemData, function (_item) {
-            const self = this;
+            var self = this;
             renderer.load(function () {
-                let result, $result;
-                let $textBlocks;
+                var result, $result;
+                var $textBlocks;
 
                 item = _item;
                 item.setRenderer(this);
@@ -174,31 +259,23 @@ define([
 
                 $(`#${containerId}`).append($result);
 
-                const plugin = pluginFactory(testRunner, testRunner.getAreaBroker());
-                testRunner.on('resize.adaptItemHeight', function () {
-                    const $contentArea = runner
-                        .getAreaBroker()
-                        .getContentArea();
-                    const $itemContainer = $contentArea.find('.text-block-wrap[data-scrolling]');
-
-                    $itemContainer.each(function () {
-                        const $item = $(this);
-                        assert.equal($item.css('overflow'), 'auto', 'The text block has styles for scrolling');
-                    });
-
-                    plugin
-                        .init()
-                        .then(function () {
-                            testRunner.trigger('resize.adaptItemHeight');
-                        })
-                        .catch(function (err) {
-                            assert.ok(false, err.message);
-                        });
-
+                var plugin = pluginFactory(testRunner, testRunner.getAreaBroker());
+                testRunner.on('renderitem', function () {
+                    console.log($textBlocks);
                     ready();
-                }, self.getLoadedClasses());
-            });
+                });
 
+                plugin
+                    .init()
+                    .then(function () {
+                        testRunner.trigger('renderitem');
+                    })
+                    .catch(function (err) {
+                        assert.ok(false, err.message);
+                        ready();
+                    });
+            }, self.getLoadedClasses());
         });
+
     });
 });

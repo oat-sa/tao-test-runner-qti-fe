037c108616456cf2a60eeafe5f4f744d66229ad8
Tamas Besenyei <tamas@besenyei.net>
2020-11-11 14:43:10 +0100

    fix: change position to label

--- src/plugins/navigation/next.js
+++ src/plugins/navigation/next.js
@@ -76,7 +76,7 @@ const updateElement = function updateElement($element, testRunner, isLast = fals
     if (dataType === 'next' && !testContext.isAdaptive && !testContext.isCatAdaptive) {
         const testMap = testRunner.getTestMap();
         const nextItem = navigationHelper.getNextItem(testMap, testContext.itemPosition);
-        $element.attr('title', __(buttonData.next.specificTitle, nextItem.position));
+        $element.attr('title', __(buttonData.next.specificTitle, nextItem.label));
     } else {
         $element.attr('title', buttonData[dataType].title);
     }

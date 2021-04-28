2c5ed2939b1656a92f12203c81d899ce66d15346
jsconan <jsconan@gmail.com>
2021-04-28 16:11:47 +0200

    refactor: convert unit test of validateResponses plugin to ES6

--- test/plugins/navigation/validateResponses/test.js
+++ test/plugins/navigation/validateResponses/test.js
@@ -22,30 +22,25 @@ define([
     'taoQtiTest/test/runner/mocks/providerMock',
     'taoQtiTest/runner/plugins/navigation/validateResponses',
     'taoQtiTest/runner/helpers/currentItem'
-], function(_, runnerFactory, providerMock, pluginFactory, currentItemHelper) {
+], function (_, runnerFactory, providerMock, pluginFactory, currentItemHelper) {
     'use strict';
 
-    var pluginApi;
-    var providerName = 'mock';
+    const providerName = 'mock';
     runnerFactory.registerProvider(providerName, providerMock());
 
     //Mock the isAnswered helper, using testRunner property
-    currentItemHelper.isAnswered = function(testRunner) {
-        return testRunner.answered;
-    };
+    currentItemHelper.isAnswered = testRunner => testRunner.answered;
 
     //Mock the getDeclarations helper, using testRunner property
-    currentItemHelper.getDeclarations = function(testRunner) {
-        return testRunner.responses;
-    };
+    currentItemHelper.getDeclarations = testRunner => testRunner.responses;
 
     /**
      * The following tests applies to all plugins
      */
     QUnit.module('pluginFactory');
 
-    QUnit.test('module', function(assert) {
-        var runner = runnerFactory(providerName);
+    QUnit.test('module', assert => {
+        const runner = runnerFactory(providerName);
 
         assert.equal(typeof pluginFactory, 'function', 'The pluginFactory module exposes a function');
         assert.equal(typeof pluginFactory(runner), 'object', 'The plugin factory produces an instance');
@@ -56,31 +51,29 @@ define([
         );
     });
 
-    pluginApi = [
-        { name: 'init', title: 'init' },
-        { name: 'render', title: 'render' },
-        { name: 'finish', title: 'finish' },
-        { name: 'destroy', title: 'destroy' },
-        { name: 'trigger', title: 'trigger' },
-        { name: 'getTestRunner', title: 'getTestRunner' },
-        { name: 'getAreaBroker', title: 'getAreaBroker' },
-        { name: 'getConfig', title: 'getConfig' },
-        { name: 'setConfig', title: 'setConfig' },
-        { name: 'getState', title: 'getState' },
-        { name: 'setState', title: 'setState' },
-        { name: 'show', title: 'show' },
-        { name: 'hide', title: 'hide' },
-        { name: 'enable', title: 'enable' },
-        { name: 'disable', title: 'disable' }
-    ];
-
-    QUnit.cases.init(pluginApi).test('plugin API ', function(data, assert) {
-        var runner = runnerFactory(providerName);
-        var timer = pluginFactory(runner);
+    QUnit.cases.init([
+        { title: 'init' },
+        { title: 'render' },
+        { title: 'finish' },
+        { title: 'destroy' },
+        { title: 'trigger' },
+        { title: 'getTestRunner' },
+        { title: 'getAreaBroker' },
+        { title: 'getConfig' },
+        { title: 'setConfig' },
+        { title: 'getState' },
+        { title: 'setState' },
+        { title: 'show' },
+        { title: 'hide' },
+        { title: 'enable' },
+        { title: 'disable' }
+    ]).test('plugin API ', (data, assert) => {
+        const runner = runnerFactory(providerName);
+        const timer = pluginFactory(runner);
         assert.equal(
-            typeof timer[data.name],
+            typeof timer[data.title],
             'function',
-            `The pluginFactory instances expose a "${data.name}" function`
+            `The pluginFactory instances expose a "${data.title}" function`
         );
     });
 
@@ -93,8 +86,8 @@ define([
                 context: {
                     itemIdentifier: 'item-1'
                 },
-                options : {
-                    enableValidateResponses: false,
+                options: {
+                    enableValidateResponses: false
                 },
                 validateResponses: true,
                 answered: false,
@@ -105,7 +98,7 @@ define([
                 context: {
                     itemIdentifier: 'item-1'
                 },
-                options : {
+                options: {
                     enableValidateResponses: true
                 },
                 validateResponses: true,
@@ -117,7 +110,7 @@ define([
                 context: {
                     itemIdentifier: 'item-1'
                 },
-                options : {
+                options: {
                     enableValidateResponses: true
                 },
                 answered: false,
@@ -129,7 +122,7 @@ define([
                 context: {
                     itemIdentifier: 'item-1'
                 },
-                options : {
+                options: {
                     enableValidateResponses: true
                 },
                 validateResponses: true,
@@ -144,24 +137,24 @@ define([
             });
             const plugin = pluginFactory(runner, runner.getAreaBroker());
 
-            runner.getCurrentItem = () => ({ validateResponses : data.validateResponses });
+            runner.getCurrentItem = () => ({ validateResponses: data.validateResponses });
 
             assert.expect(1);
 
             plugin
                 .init()
-                .then(function() {
+                .then(() => {
                     runner.setTestContext(data.context);
                     runner.answered = data.answered;
                     runner.responses = data.responses;
 
-                    runner.on('move', function() {
+                    runner.on('move', () => {
                         assert.ok(true, 'Move is allowed');
                         ready();
                     });
                     runner.trigger('move');
                 })
-                .catch(function(err) {
+                .catch(err => {
                     assert.ok(false, err.message);
                     ready();
                 });
@@ -174,7 +167,7 @@ define([
                 context: {
                     itemIdentifier: 'item-1'
                 },
-                options : {
+                options: {
                     enableValidateResponses: true
                 },
                 validateResponses: true,
@@ -182,33 +175,33 @@ define([
                 responses: ['foo']
             }
         ])
-        .test('Moving is prevented ', function(data, assert) {
+        .test('Moving is prevented ', (data, assert) => {
             const ready = assert.async();
 
             const runner = runnerFactory(providerName, {}, {
-                options : data.options
+                options: data.options
             });
             const plugin = pluginFactory(runner, runner.getAreaBroker());
 
             runner.getCurrentItem = () => ({
-                validateResponses : data.validateResponses,
-                answered : data.answered
+                validateResponses: data.validateResponses,
+                answered: data.answered
             });
 
             assert.expect(2);
 
             plugin
                 .init()
-                .then(function() {
+                .then(() => {
                     runner.setTestContext(data.context);
                     runner.answered = data.answered;
                     runner.responses = data.responses;
 
-                    runner.on('move', function() {
+                    runner.on('move', () => {
                         assert.ok(false, 'Move is denied');
                         ready();
                     });
-                    runner.off('alert.notallowed').on('alert.notallowed', function(message, cb) {
+                    runner.off('alert.notallowed').on('alert.notallowed', (message, cb) => {
                         assert.equal(
                             message,
                             'A valid response to this item is required.',
@@ -216,13 +209,13 @@ define([
                         );
                         cb();
                     });
-                    runner.on('resumeitem', function() {
+                    runner.on('resumeitem', () => {
                         assert.ok(true, 'Move has been prevented');
                         ready();
                     });
                     runner.trigger('move');
                 })
-                .catch(function(err) {
+                .catch(err => {
                     assert.ok(false, err.message);
                     ready();
                 });
@@ -235,10 +228,10 @@ define([
                 context: {
                     itemIdentifier: 'item-1'
                 },
-                options :  {
-                    enableValidateResponses: true,
+                options: {
+                    enableValidateResponses: true
                 },
-                pluginConfig : {
+                pluginConfig: {
                     validateOnPreviousMove: false
                 },
                 validateResponses: true,
@@ -246,31 +239,31 @@ define([
                 responses: ['foo']
             }
         ])
-        .test('Moving backwards is also allowed ', (data, assert)  => {
+        .test('Moving backwards is also allowed ', (data, assert) => {
             const ready = assert.async();
             const runner = runnerFactory(providerName, {}, {
                 options: data.options
             });
             const plugin = pluginFactory(runner, runner.getAreaBroker(), data.pluginConfig);
 
-            runner.getCurrentItem = () => ({ validateResponses : data.validateResponses });
+            runner.getCurrentItem = () => ({ validateResponses: data.validateResponses });
 
             assert.expect(1);
 
             plugin
                 .init()
-                .then(function() {
+                .then(() => {
                     runner.setTestContext(data.context);
                     runner.answered = data.answered;
                     runner.responses = data.responses;
 
-                    runner.on('move', function() {
+                    runner.on('move', () => {
                         assert.ok(true, 'Moving is allowed');
                         ready();
                     });
                     runner.previous();
                 })
-                .catch(function(err) {
+                .catch(err => {
                     assert.ok(false, err.message);
                     ready();
                 });

8763a7f16c4c5c3bd148dc3018fff66759be5e5c
jsconan <jsconan@gmail.com>
2021-05-03 16:26:50 +0200

    chore: fix lint warning in unit test for the allowSkipping plugin

--- test/plugins/navigation/allowSkipping/test.js
+++ test/plugins/navigation/allowSkipping/test.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2017-2021 (original work) Open Assessment Technologies SA ;
  */
 
 define([
@@ -35,6 +35,7 @@ define([
 
     /**
      * Gets a configured instance of the Test Runner
+     * @param {Object} [config] - Optional config to setup the test runner
      * @returns {Promise<runner>}
      */
     function getTestRunner(config) {

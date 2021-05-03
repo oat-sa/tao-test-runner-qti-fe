cd3110ec8fd06f474fe5252625db2d4650f07e25
jsconan <jsconan@gmail.com>
2021-05-03 16:18:35 +0200

    chore: fix lint warning in unit test for the answerMasking plugin

--- test/plugins/tools/answerMasking/plugin/test.js
+++ test/plugins/tools/answerMasking/plugin/test.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017-2019 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2017-2021 (original work) Open Assessment Technologies SA ;
  */
 /**
  * @author Christophe Noël <christophe@taotesting.com>
@@ -59,6 +59,7 @@ define([
 
     /**
      * Gets a configured instance of the Test Runner
+     * @param {Object} [config] - Optional config to setup the test runner
      * @returns {Promise<runner>}
      */
     function getTestRunner(config) {

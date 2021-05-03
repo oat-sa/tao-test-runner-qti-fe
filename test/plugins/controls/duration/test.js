9344e1bef30f4630a5fc60254d508964a46b98ab
jsconan <jsconan@gmail.com>
2021-05-03 16:30:00 +0200

    chore: fix lint warning in unit test for the duration plugin

--- test/plugins/controls/duration/test.js
+++ test/plugins/controls/duration/test.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA
+ * Copyright (c) 2020-2021 (original work) Open Assessment Technologies SA
  */
 
 define([
@@ -59,6 +59,7 @@ define([
 
     /**
      * Gets a configured instance of the Test Runner
+     * @param {Object} [config] - Optional config to setup the test runner
      * @returns {Promise<runner>}
      */
     function getTestRunner(config) {

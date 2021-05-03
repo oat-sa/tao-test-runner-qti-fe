fbc589bf3aeca8ffb20e7c6604b371759ebad4ac
jsconan <jsconan@gmail.com>
2021-05-03 16:29:28 +0200

    chore: fix lint warning in unit test for the progressbar plugin

--- test/plugins/controls/progressbar/plugin/test.js
+++ test/plugins/controls/progressbar/plugin/test.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2018 (original work) Open Assessment Technologies SA
+ * Copyright (c) 2018-2021 (original work) Open Assessment Technologies SA
  */
 
 define([
@@ -29,6 +29,7 @@ define([
 
     /**
      * Gets a configured instance of the Test Runner
+     * @param {Object} [config] - Optional config to setup the test runner
      * @returns {Promise<runner>}
      */
     function getTestRunner(config) {

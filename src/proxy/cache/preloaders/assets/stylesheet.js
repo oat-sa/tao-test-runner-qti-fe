83894e53333849c1812130bdf3602e02d05494d1
jsconan <jsconan@gmail.com>
2021-10-06 13:23:42 +0200

    feat: extract the stylesheet asset preloader

---
+++ src/proxy/cache/preloaders/assets/stylesheet.js
@@ -0,0 +1,125 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017-2021 Open Assessment Technologies SA
+ */
+
+/**
+ * (Pre)load stylesheets.
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+
+import _ from 'lodash';
+
+/**
+ * Test the support of possible `<link rel>` values.
+ * @param {string} feature - the value to test
+ * @returns {boolean}
+ * @private
+ */
+const relSupport = feature => {
+    const fakeLink = document.createElement('link');
+    try {
+        if (fakeLink.relList && 'function' === typeof fakeLink.relList.supports) {
+            return fakeLink.relList.supports(feature);
+        }
+    } catch (err) {
+        return false;
+    }
+};
+
+/**
+ * Does the current env supports `<link ref="preload">`
+ * @type {boolean}
+ * @private
+ */
+const supportPreload = relSupport('preload');
+
+/**
+ * Does the current env supports `<link ref="prefetch">`
+ * @type {boolean}
+ * @private
+ */
+const supportPrefetch = relSupport('prefetch');
+
+/**
+ * Manages the preloading of stylesheets
+ * @returns {assetPreloader}
+ */
+export default function stylesheetPreloaderFactory() {
+    //keep references to preloaded CSS files
+    const stylesheets = {};
+
+    return {
+        /**
+         * The name of the preloader
+         * @type {string}
+         */
+        name: 'css',
+
+        /**
+         * Preloads a stylesheet
+         * @param {string} url - the url of the stylesheet to preload
+         * @param {string} sourceUrl - the unresolved URL (used to index)
+         * @param {string} itemIdentifier - the id of the item the asset belongs to
+         * @returns {Promise}
+         */
+        load(url, sourceUrl, itemIdentifier) {
+            stylesheets[itemIdentifier] = stylesheets[itemIdentifier] || {};
+
+            if (!stylesheets[itemIdentifier][sourceUrl]) {
+                const link = document.createElement('link');
+                if (supportPreload) {
+                    link.setAttribute('rel', 'preload');
+                    link.setAttribute('as', 'style');
+                } else if (supportPrefetch) {
+                    link.setAttribute('rel', 'prefetch');
+                    link.setAttribute('as', 'style');
+                } else {
+                    link.disabled = true;
+                    link.setAttribute('rel', 'stylesheet');
+                    link.setAttribute('type', 'text/css');
+                }
+                link.setAttribute('data-preload', true);
+                link.setAttribute('href', url);
+
+                document.querySelector('head').appendChild(link);
+                stylesheets[itemIdentifier][sourceUrl] = link;
+            }
+            return Promise.resolve();
+        },
+
+        /**
+         * Removes the prefetched stylesheet
+         * @param {string} url - the url of the stylesheet to unload
+         * * @param {string} sourceUrl - the unresolved URL (used to index)
+         * @param {string} itemIdentifier - the id of the item the asset belongs to
+         * @returns {Promise}
+         */
+        unload(url, sourceUrl, itemIdentifier) {
+            if (stylesheets[itemIdentifier]) {
+                const link =
+                    stylesheets[itemIdentifier][sourceUrl] ||
+                    document.querySelector(`head link[data-preload][href="${url}"]`);
+                if (link) {
+                    document.querySelector('head').removeChild(link);
+                }
+                stylesheets[itemIdentifier] = _.omit(stylesheets[itemIdentifier], sourceUrl);
+            }
+            return Promise.resolve();
+        }
+    };
+}
---
+++ test/proxy/cache/preloaders/assets/stylesheet/test.js
@@ -0,0 +1,90 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 Open Assessment Technologies SA ;
+ */
+
+/**
+ * Test of taoQtiTest/runner/proxy/cache/preloaders/assets/stylesheet
+ */
+define(['taoQtiTest/runner/proxy/cache/preloaders/assets/stylesheet'], function (stylesheetPreloaderFactory) {
+    'use strict';
+
+    QUnit.module('API');
+
+    QUnit.test('module', assert => {
+        assert.expect(3);
+
+        assert.equal(typeof stylesheetPreloaderFactory, 'function', 'The module exposes a function');
+        assert.equal(typeof stylesheetPreloaderFactory(), 'object', 'The module is a factory');
+        assert.notDeepEqual(
+            stylesheetPreloaderFactory(),
+            stylesheetPreloaderFactory(),
+            'The factory creates new instances'
+        );
+    });
+
+    QUnit.test('property [name]', assert => {
+        assert.expect(1);
+        const preloader = stylesheetPreloaderFactory();
+
+        assert.strictEqual(preloader.name, 'css', 'The preloader has the name "css"');
+    });
+
+    QUnit.cases.init([{ title: 'load' }, { title: 'unload' }]).test('method ', (data, assert) => {
+        assert.expect(1);
+        const preloader = stylesheetPreloaderFactory();
+
+        assert.equal(typeof preloader[data.title], 'function', `The preloader has the method ${data.title}`);
+    });
+
+    QUnit.module('behavior');
+
+    QUnit.test('load/unload', assert => {
+        assert.expect(3);
+        const ready = assert.async();
+        const preloader = stylesheetPreloaderFactory();
+        const data = {
+            asset: 'sample.css',
+            itemIdentifier: 'item-1'
+        };
+        const assetUrl = `./${data.asset}`;
+
+        assert.strictEqual(
+            document.querySelector(`head link[href="${assetUrl}"]`),
+            null,
+            'The asset is not yet loaded'
+        );
+
+        preloader
+            .load(assetUrl, data.asset, data.itemIdentifier)
+            .then(() => {
+                assert.ok(
+                    document.querySelector(`head link[href="${assetUrl}"]`) instanceof HTMLLinkElement,
+                    'The asset has been preloaded'
+                );
+            })
+            .then(() => preloader.unload(assetUrl, data.asset, data.itemIdentifier))
+            .then(() => {
+                assert.strictEqual(
+                    document.querySelector(`head link[href="${assetUrl}"]`),
+                    null,
+                    'The asset has been unloaded'
+                );
+            })
+            .catch(err => assert.ok(false, err))
+            .then(ready);
+    });
+});

1360dc361cfea21d7f11fd4c2e4f2af043358d35
jsconan <jsconan@gmail.com>
2021-10-06 13:21:55 +0200

    feat: extract the image asset preloader

---
+++ src/proxy/cache/preloaders/assets/image.js
@@ -0,0 +1,73 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017-2021 Open Assessment Technologies SA
+ */
+
+/**
+ * (Pre)load images.
+ *
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+
+import _ from 'lodash';
+
+/**
+ * Manages the preloading of images
+ * @returns {assetPreloader}
+ */
+export default function imagePreloaderFactory() {
+    //keep references to preloaded images attached
+    //in order to prevent garbage collection of cached images
+    const images = {};
+
+    return {
+        /**
+         * The name of the preloader
+         * @type {string}
+         */
+        name: 'img',
+
+        /**
+         * Preloads an image, using the in memory Image object
+         * @param {string} url - the url of the image to preload
+         * @param {string} sourceUrl - the unresolved URL (used to index)
+         * @param {string} itemIdentifier - the id of the item the asset belongs to
+         * @returns {Promise}
+         */
+        load(url, sourceUrl, itemIdentifier) {
+            images[itemIdentifier] = images[itemIdentifier] || {};
+            if ('Image' in window && !images[itemIdentifier][sourceUrl]) {
+                images[itemIdentifier][sourceUrl] = new Image();
+                images[itemIdentifier][sourceUrl].src = url;
+            }
+            return Promise.resolve();
+        },
+
+        /**
+         * Removes images ref so they can be garbage collected
+         * @param {string} url - the url of the image to unload
+         * @param {string} sourceUrl - the unresolved URL (used to index)
+         * @param {string} itemIdentifier - the id of the item the asset belongs to
+         * @returns {Promise}
+         */
+        unload(url, sourceUrl, itemIdentifier) {
+            if (images[itemIdentifier]) {
+                images[itemIdentifier] = _.omit(images[itemIdentifier], sourceUrl);
+            }
+            return Promise.resolve();
+        }
+    };
+}
---
+++ test/proxy/cache/preloaders/assets/image/test.js
@@ -0,0 +1,83 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 Open Assessment Technologies SA ;
+ */
+
+/**
+ * Test of taoQtiTest/runner/proxy/cache/preloaders/assets/image
+ */
+define(['taoQtiTest/runner/proxy/cache/preloaders/assets/image'], function (imagePreloaderFactory) {
+    'use strict';
+
+    QUnit.module('API');
+
+    QUnit.test('module', assert => {
+        assert.expect(3);
+
+        assert.equal(typeof imagePreloaderFactory, 'function', 'The module exposes a function');
+        assert.equal(typeof imagePreloaderFactory(), 'object', 'The module is a factory');
+        assert.notDeepEqual(imagePreloaderFactory(), imagePreloaderFactory(), 'The factory creates new instances');
+    });
+
+    QUnit.test('property [name]', assert => {
+        assert.expect(1);
+        const preloader = imagePreloaderFactory();
+
+        assert.strictEqual(preloader.name, 'img', 'The preloader has the name "img"');
+    });
+
+    QUnit.cases.init([{ title: 'load' }, { title: 'unload' }]).test('method ', (data, assert) => {
+        assert.expect(1);
+        const preloader = imagePreloaderFactory();
+
+        assert.equal(typeof preloader[data.title], 'function', `The preloader has the method ${data.title}`);
+    });
+
+    QUnit.module('behavior');
+
+    QUnit.test('load/unload', assert => {
+        assert.expect(4);
+        const ready = assert.async();
+        const preloader = imagePreloaderFactory();
+        const data = {
+            asset: 'sample.jpg',
+            itemIdentifier: 'item-1'
+        };
+        const assetUrl = `./${data.asset}`;
+
+        //Hack the Image element to assert the load
+        window.Image = function () {
+            assert.ok(true, 'A new image is created');
+        };
+        Object.defineProperty(window.Image.prototype, 'src', {
+            set: function src(url) {
+                assert.equal(url, assetUrl, 'The image has been preloaded');
+            }
+        });
+
+        preloader
+            .load(assetUrl, data.asset, data.itemIdentifier)
+            .then(() => {
+                assert.ok(true, 'The asset has been preloaded');
+            })
+            .then(() => preloader.unload(assetUrl, data.asset, data.itemIdentifier))
+            .then(() => {
+                assert.ok(true, 'The asset has been unloaded');
+            })
+            .catch(err => assert.ok(false, err))
+            .then(ready);
+    });
+});
